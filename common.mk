e221497e5a47b00f578debd0b0489f7aacb3327d
AmritoRock <amritokarmokar5i@gmail.com>
2024-01-21 10:13:20 +0600

    sm8350-common: Build libloc_net_iface vendor variant
    E LocSvc_misc_utils: logDlError:129] dlopen error: dlopen failed: library "libloc_net_iface.so" not found
    Signed-off-by: Amritorock <amritokarmokar5i@gmail.com>
    Signed-off-by: mvimal2607 <mvimal2607@gmail.com>

--- common.mk
+++ common.mk
@@ -198,7 +198,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gnss.measurement_corrections@1.1.vendor \
     android.hardware.gnss.visibility_control@1.0.vendor \
-    android.hardware.gnss@2.1.vendor
+    android.hardware.gnss@2.1.vendor \
+    libloc_net_iface.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:$(TARGET_COPY_OUT_ODM)/etc/gps.conf
